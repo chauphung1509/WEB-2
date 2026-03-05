fee55494235f100303fab36102150202d3bc860a
chauphung1509 <kimchauphung1509@gmail.com>
2026-03-05 10:52:37 +0700

    Update momo.ts

--- momo/momo.ts
+++ momo/momo.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-momo',
-  imports: [],
+  standalone: false,
   templateUrl: './momo.html',
   styleUrl: './momo.css',
 })
 export class Momo {
+  amount: number = 0;
 
+  constructor(private http: HttpClient) {}
+
+  payWithMomo() {
+    this.http.post<any>("http://localhost:3002/payment/momo", {
+      amount: this.amount
+    })
+    .subscribe(res => {
+
+      console.log("Saved to DB, response:", res);
+
+      if (res.payUrl) {
+        window.location.href = res.payUrl;
+      }
+    });
+  }
 }
